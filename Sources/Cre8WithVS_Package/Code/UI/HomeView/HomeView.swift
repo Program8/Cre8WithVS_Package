fd9af3b9ededf9392389e8869d75cfdb6c16fce7
vijaysachan_sdn <vijaysachan@smartdatainc.net>
2025-06-16 15:39:10 +0530

    --

--- Sources/Cre8WithVS_Package/Code/UI/HomeView/HomeView.swift
+++ Sources/Cre8WithVS_Package/Code/UI/HomeView/HomeView.swift
@@ -5,37 +5,35 @@
 //  Created by Vijay Sachan on 26/08/23.
 //
 import SwiftUI
-public struct HomeView: View {    
+public struct HomeView: View {
     @StateObject private var viewModel = HomeViewModel()
     public init(){}
     public var body: some View {
-            AppView(code: viewModel){
-                List{
-                    ForEach(ListCategory.allCases) { category in
-                        Section(header: Text(category.rawValue)) {
-                            ForEach(viewModel.filteredItems.filter { $0.category == category }) { item in
-                                NavigationLink(destination: item.contentView().navigationTitle(item.title)) {
-                                    VStack(alignment: .leading) {
-                                        Text(item.title)
-                                            .font(.headline)
-                                        Text(item.subTitle)
-                                            .font(.caption)
-                                            .foregroundColor(.gray)
-                                    }
+        AppView(code: viewModel){
+            List{
+                ForEach(ListCategory.allCases) { category in
+                    Section(header: Text(category.rawValue).pkg_FontHeader()) {
+                        ForEach(viewModel.filteredItems.filter { $0.category == category }) { item in
+                            NavigationLink(destination: item.contentView().navigationTitle(item.title)) {
+                                VStack(alignment: .leading) {
+                                    Text(item.title).pkg_FontTitle()
+                                    Text(item.subTitle)
+                                        .pkg_FontSubTitle()
+                                        .foregroundColor(.gray)
                                 }
                             }
                         }
                     }
                 }
-                
-            }.navigationTitle("SwiftUI")
-                .navigationBarTitleDisplayMode(.large)
-                .searchable(text: $viewModel.searchText)
-                // .inline mode will put the title in center in navigation bar
-                // .large mode will show large title and when list is scrolled,mode will automatically set to .inline mode
-                // .automatic Inherit the display mode from the previous navigation item
-        }
+            }
+        }.navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.large)
+            .searchable(text: $viewModel.searchText).pkg_FontSubTitle()
+        // .inline mode will put the title in center in navigation bar
+        // .large mode will show large title and when list is scrolled,mode will automatically set to .inline mode
+        // .automatic Inherit the display mode from the previous navigation item
     }
+}
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
